256b329a521c273300935dab0fbec795bd178361
Pascal Berrang <mail@paberr.net>
2018-08-06 15:16:59 +0200

    Expose namespaced types

---
+++ types/RpcClient.d.ts
@@ -0,0 +1,8 @@
+import { RpcClient as Client, PostMessageRpcClient, RedirectRpcClient, ResponseHandler } from '../dist/RpcClient';
+
+export declare const RpcClient: {
+    RpcClient: typeof Client;
+    PostMessageRpcClient: typeof PostMessageRpcClient;
+    RedirectRpcClient: typeof RedirectRpcClient;
+    ResponseHandler: ResponseHandler;
+};
