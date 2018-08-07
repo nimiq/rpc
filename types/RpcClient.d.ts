5565b93a7dcde2c06a80d7524ed7692438587e50
Pascal Berrang <mail@paberr.net>
2018-08-07 12:17:01 +0200

    Improve build process

--- types/RpcClient.d.ts
+++
@@ -1,8 +0,0 @@
-import { RpcClient as Client, PostMessageRpcClient, RedirectRpcClient, ResponseHandler } from '../dist/RpcClient';
-
-export declare const RpcClient: {
-    RpcClient: typeof Client;
-    PostMessageRpcClient: typeof PostMessageRpcClient;
-    RedirectRpcClient: typeof RedirectRpcClient;
-    ResponseHandler: ResponseHandler;
-};
--- types/RpcServer.d.ts
+++
@@ -1,8 +0,0 @@
-import { RpcServer as Server, CommandHandler, ResponseStatus, State } from '../dist/RpcServer';
-
-export declare const RpcServer: {
-    RpcServer: typeof Server;
-    CommandHandler: CommandHandler;
-    ResponseStatus: typeof ResponseStatus;
-    State: typeof State;
-};
